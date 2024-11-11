60411cc3588f1340cff589866626baabef930ee8
Phong Nguyễn <vanphong02062002@gmail.com>
2024-11-12 00:07:15 +0700

    add tailwind

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import './utils/styles/global.css';
+
+const App = () => {
+    return (
+        <View className="bg-black w-full h-full">
+            <Text className="text-white text-bold text-2xl">Axcxpp</Text>
+        </View>
+    );
+};
+
+export default App;
